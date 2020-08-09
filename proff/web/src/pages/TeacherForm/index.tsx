404908e42ab427bb306cd7ecd331c173ed1adb08
Carlos Araki <carlosarak@gmail.com>
2020-08-09 19:40:09 -0300

    style:alteracao de css para pagina de formulário

--- proff/web/src/pages/TeacherForm/index.tsx
+++ proff/web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 
+import warnigIcon from '../../assets/images/icons/warning.svg';
+
 import './styles.css'
 
 function TeacherForm() {
@@ -18,6 +20,21 @@ function TeacherForm() {
                 <Input  name="avatar" label="Avatar"/>
                 <Input  name="whatsapp" label="Whatsapp"/>
               </fieldset>
+              <fieldset>
+                <legend> Sobre a aula</legend>
+                <Input  name="subject" label="Matéria"/>
+                <Input  name="cost" label="Custo da aula"/>
+              </fieldset>
+              <footer>
+                <p>
+                  <img 
+                    src = {warnigIcon} alt ="Aviso Importante"
+                  />
+                  Importante ! <br />
+                  Preencha todos os dados !
+                </p>
+                <button type='button'>Salvar formulário</button>
+              </footer>
             </main>
       </div>
     );
